ba35dc1ae398da9ae70147a84d6b2c6a186e0d63
Jimmy Ng <jimmy_ng@outlook.de>
2023-05-11 15:07:41 +0200

    adds solution to code task 02

---
+++ coding-task-02/script.js
@@ -0,0 +1,35 @@
+//Task 01
+
+console.log("---This is the result to Task 01---");
+console.log("J" + "i" + "m" + "m" + "y" + " " + "N" + "g");
+
+console.log("---This is the result to Task 02---");
+//Task 02
+//Error 1
+// Error! Make it work
+console.log("Hallo");
+
+//Error 2
+// Error! Make it work
+console.log("That doesn't work" + " - " + "Now it does, dummy");
+
+//Error 3
+// should return true. Change only the operator to fix it.
+console.log(!(50 + 2 > 52));
+
+//Error 4
+// should be false. Change only the operator to fix it.
+console.log("333" === 333);
+
+//Error 5
+// Wrong result: Expected 555 not 855. Change only the operator to fix it.
+console.log((600 + 510) / 2);
+
+//Error 6
+// Wrong result: Expected a Number 4 received 22
+console.log(2 + 2);
+
+console.log("---This is the result to Task 03---");
+console.log(25 / 5); // Result: 5
+console.log(50 / 10); // Result: 5
+console.log(25 / 5 === 5 && 50 / 10 === 5);
